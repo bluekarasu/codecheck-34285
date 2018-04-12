fe94b0c167cb5391f8faa81feaea34ebbdb13547
bluekarasu <tumka0617@gmail.com>
2018-04-12 18:37:04 +0900

    Update test_optional.js

--- test/test_optional.js
+++ test/test_optional.js
@@ -3,11 +3,9 @@ var expect = require('expect');
 var run = require('./lib.js').run;
 
 describe('Creative Engineer Exam #1 (Open Tests)', () => {
-	it('Case #1 should be caluculated', () => co(function*() {
+	it('Case #11 should be caluculated', () => co(function*() {
 		const input = [
-			'2017/02',
-			'2017/02/01 08:00-12:00 13:00-16:00',
-			'\n'
+			'2017/02'
 		];
 		const expected = [
 			'0', '0', '0', '0', '0'
@@ -16,118 +14,15 @@ describe('Creative Engineer Exam #1 (Open Tests)', () => {
 		expect(actual).toEqual(expected);
 	}));
 
-	it('Case #2 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/01 08:00-12:00 13:00-17:00',
-			'\n'
-		];
-		const expected = [
-			'1', '0', '0', '0', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
+	
 
-	it('Case #3 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/01 08:00-12:00 13:00-21:00',
-			'\n'
-		];
-		const expected = [
-			'1', '4', '0', '0', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #4 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/01 10:00-12:00 13:00-21:00',
-			'\n'
-		];
-		const expected = [
-			'3', '2', '0', '0', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #5 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/01 08:00-12:00 13:00-26:00',
-			'\n'
-		];
-		const expected = [
-			'1', '9', '4', '0', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #6 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/04 08:00-12:00 13:00-23:00',
-			'\n'
-		];
-		const expected = [
-			'0', '0', '1', '14', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #7 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/03 08:00-12:00 13:00-26:00',
-			'\n'
-		];
-		const expected = [
-			'1', '7', '4', '2', '0'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #8 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/05 08:00-12:00 13:00-23:00',
-			'\n'
-		];
-		const expected = [
-			'0', '0', '1', '0', '14'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #9 should be caluculated', () => co(function*() {
-		const input = [
-			'2017/02',
-			'2017/02/04 08:00-12:00 13:00-26:00',
-			'\n'
-		];
-		const expected = [
-			'0', '0', '4', '15', '2'
-		];
-		const actual = yield run('./run.sh', [], input.join('\n'));
-		expect(actual).toEqual(expected);
-	}));
-
-	it('Case #10 should be caluculated', () => co(function*() {
+	it('Case #12 should be caluculated', () => co(function*() {
 		const input1 = [
 			'2017/01',
 			'2017/01/16 08:00-12:00 13:00-18:00',
 			'2017/01/17 08:00-12:00 13:00-18:00',
 			'2017/01/18 08:00-12:00 13:00-18:00',
-			'2017/01/19 08:00-12:00 13:00-17:00',
-			'\n'
+			'2017/01/19 08:00-12:00 13:00-17:00'
 		];
 		const expected1 = [
 			'4', '3', '0', '0', '0'
